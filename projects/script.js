55b83be5c0a76a1b12934f91fa57040a00a315e9
Sudip Pawar <127401315+Sudippawar89@users.noreply.github.com>
2025-02-23 15:28:41 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -5,21 +5,22 @@ function showProjects(projects) {
         projectsHTML += `
         <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-     
-      
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
+        
+        <img draggable="false" src="${project.image}" alt="project image" />
+        
+        <div class="content">
+            <div class="tag">
+                <h3>${project.name}</h3>
+            </div>
+            <div class="desc">
+                <p>${project.desc}</p>
+                <div class="btns">
+                    <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                </div>
+            </div>
         </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-          </div>
         </div>
-      </div>
-    </div>
-    </div>`;
+        </div>`;
     });
     projectsContainer.innerHTML = projectsHTML;
 }
