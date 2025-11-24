4d330a2be1930eb5934adf8b5ddb673b3335c50c
Sudip Pawar <127401315+Sudippawar89@users.noreply.github.com>
2025-11-25 00:11:24 +0530

    25/11/2025

---
+++ assets/images/script.js
@@ -0,0 +1,29 @@
+function showProjects(projects) {
+    let projectsContainer = document.querySelector(".work .box-container");
+    let projectsHTML = ""; 
+    projects.forEach(project => {
+        // Directly use the image path from JSON (No additional concatenation)
+        let imageUrl = project.image; 
+
+        projectsHTML += `
+        <div class="grid-item ${project.category}">
+            <div class="box tilt" style="width: 380px; margin: 1rem">
+                
+                <img draggable="false" src="${imageUrl}" alt="${project.name}" />
+                
+                <div class="content">
+                    <div class="tag">
+                        <h3>${project.name}</h3>
+                    </div>
+                    <div class="desc">
+                        <p>${project.desc}</p>
+                        <div class="btns">
+                            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    });
+    projectsContainer.innerHTML = projectsHTML;
+}
